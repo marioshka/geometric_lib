cc0910ebd1e6526a2cf02ecd5274628e283efdae
Isaenkowa Maria <466051@niuitmo.ru>
2024-11-23 21:08:48 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,7 +1,10 @@
-
 def area(a):
+    if a < 0:
+        raise AssertionError("a не может быть отрицательной")
     return a * a
 
 
 def perimeter(a):
+    if a < 0:
+        raise AssertionError("a не может быть отрицательной")
     return 4 * a
